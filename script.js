92d6340ec852fd37d3619ef5a1ea5e9e9923549e
tobiastermer <tobiastermer@gmx.de>
2023-04-30 10:59:38 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie wird ein Link in HTML erstellt?",
+        "answer_1": "Mit dem <a>-Tag",
+        "answer_2": "Mit dem <link>-Tag",
+        "answer_3": "Mit dem <img>-Tag",
+        "answer_4": "Mit dem <button>-Tag",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist der Zweck des <head>-Tags in HTML?",
+        "answer_1": "Um den Hauptinhalt der Webseite zu definieren",
+        "answer_2": "Um eine Liste von Links zur Verfügung zu stellen",
+        "answer_3": "Um Informationen über die Webseite bereitzustellen",
+        "answer_4": "Um das Layout der Webseite zu definieren",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie erstellt man eine Liste in HTML?",
+        "answer_1": "Mit dem <list>-Tag",
+        "answer_2": "Mit dem <ul>-Tag",
+        "answer_3": "Mit dem <table>-Tag",
+        "answer_4": "Mit dem <form>-Tag",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie fügt man ein Bild in eine HTML-Seite ein?",
+        "answer_1": "Mit dem <image>-Tag",
+        "answer_2": "Mit dem <img>-Tag",
+        "answer_3": "Mit dem <picture>-Tag",
+        "answer_4": "Mit dem <link>-Tag",
+        "right_answer": 2
+    }
+];
