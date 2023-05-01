e60599eab1d115ad4c4c051155ba81d02caeaf47
tobiastermer <tobiastermer@gmx.de>
2023-05-01 20:41:52 +0200

    endscreen, progressbar and game restart

--- content.js
+++ content.js
@@ -15,6 +15,15 @@ function generateHTMLCardFooter(currentQuestion, length) {
 
     return `<div class="question-footer">
                 <p><b>${currentQuestion}</b> von ${length} Fragen</p>
-                <button onclick="nextQuestion(); return false" class="btn btn-primary">Nächste Frage</button>
+                <button onclick="nextQuestion(); return false" class="btn btn-primary" id="next-button" disabled>Nächste Frage</button>
             </div>`
+}
+
+function generateHTMLCardEndScreen() {
+    return `<div class="fs-38 text-center">Quiz beendet!</div>
+        <p class="text-center">Du hast ${rightQuestions} von ${questions.length} Fragen richtig beantwortet.</p>
+        <div class="button-center">
+            <button onclick="restartGame(); return false" class="btn btn-primary" id="restart-button">Erneut spielen</button>
+        </div>
+    `
 }
--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 let currentQuestion = 0;
+let questionAnswered = false;
+let rightQuestions = 0;
 
 function init() {
     showCard(currentQuestion);
 }
 
 function showCard(currentQuestion) {
-    let card = document.getElementById('card');
-    card.innerHTML = '';
+    questionAnswered = false;
 
+    let card = document.getElementById('card');
     const question = questions[currentQuestion]['question'];
     const answers = [
-        questions[currentQuestion]['answer_1'], 
-        questions[currentQuestion]['answer_2'], 
-        questions[currentQuestion]['answer_3'], 
+        questions[currentQuestion]['answer_1'],
+        questions[currentQuestion]['answer_2'],
+        questions[currentQuestion]['answer_3'],
         questions[currentQuestion]['answer_4']
     ];
     const length = questions.length;
 
+    // showProgressBar();
+
+    card.innerHTML = '';
     card.innerHTML += generateHTMLCardQuestion(question);
     for (i = 0; i < answers.length; i++) {
         const answerNumber = i + 1;
@@ -26,19 +31,62 @@ function showCard(currentQuestion) {
     card.innerHTML += generateHTMLCardFooter(currentQuestion, length);
 }
 
-function answerQuestion(question) {
-    let card = document.getElementById(`answer-${question}`);
+function answerQuestion(answerNumber) {
+    let card = document.getElementById(`answer-${answerNumber}`);
     const right_answer = questions[currentQuestion]['right_answer'];
 
-    if (question == right_answer) {
-        card.classList.add('quiz-answer-right');
-    } else {
-        card.classList.add('quiz-answer-wrong');
-        document.getElementById(`answer-${right_answer}`).classList.add('quiz-answer-right');
+    if (questionAnswered == false) {
+        if (answerNumber == right_answer) {
+            card.classList.add('bg-success');
+            rightQuestions++;
+        } else {
+            card.classList.add('bg-danger');
+            document.getElementById(`answer-${right_answer}`).classList.add('bg-success');
+        }
+        blockHoverEffects();
+        document.getElementById('next-button').disabled = false;
+        showProgressBar();
     }
+}
+
+function blockHoverEffects() {
+    for (i = 1; i <= 4; i++) {
+        document.getElementById(`answer-${i}`).classList.remove('quiz-answer-card');
+    }
+    questionAnswered = true;
 }
 
 function nextQuestion() {
     currentQuestion = currentQuestion + 1
+    if (currentQuestion < questions.length) {
+        showCard(currentQuestion);
+    } else {
+        showEndScreen();
+    }
+}
+
+function showEndScreen() {
+    document.getElementById('card-image').src = './img/win.png';
+    let card = document.getElementById('card');
+    card.innerHTML = '';
+    card.innerHTML += generateHTMLCardEndScreen();
+}
+
+function showProgressBar() {
+    let progress;
+    if (questionAnswered == true) {
+        progress = Math.round((currentQuestion + 1) / questions.length * 100);
+    } else {
+        progress = Math.round((currentQuestion) / questions.length * 100);
+    }
+    document.getElementById('progress-bar').style = `width: ${progress}%`;
+    document.getElementById('progress-bar').innerHTML = `${progress}%`;
+}
+
+function restartGame() {
+    currentQuestion = 0;
+    rightQuestions = 0;
+    document.getElementById('card-image').src = './img/background.jpg';
     showCard(currentQuestion);
+    showProgressBar();
 }
